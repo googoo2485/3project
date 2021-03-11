adb54a8b7c53ed4e4601dc3d81c8d1c05bee02e8
sanggoo2485 <sanggoo2485@gmail.com>
2021-03-11 09:42:32 +0900

    no message

--- routes/mysql.js
+++ routes/mysql.js
@@ -1,13 +1,11 @@
 const mysql = require('mysql');
 const client = mysql.createConnection({
-    host: 'sanggoo2485.cafe24app.com ',
+    host: 'sanggoo2485.cafe24app.com',
     user: 'tkdrn2485',
     password: 'rntmf147.@',
     database: 'tkdrn2485',
     port: '3306',
 });
-
-
 
 
 
