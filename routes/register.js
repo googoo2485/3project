739e74e19a9d2bdd7427233f1636d0e1d9246a11
sanggoo2485 <sanggoo2485@gmail.com>
2021-03-11 10:04:20 +0900

    no message

--- routes/register.js
+++ routes/register.js
@@ -20,22 +20,21 @@ router.get('/', function(req, res, next) {
 //     "created":req.body.created
 //   }
 // }
+
 //client.connect();
+
 // let sql = "INSERT INTO user(id,pw,name,email,created)VALUSES(?,?,?,?,?)";
 // client.query(sql,(req,res)=>{
 // })
 
-// router.get('/register', function (req, res) {
-//   var body = req.body;
-//   console.log(body);
+router.get('/', function (req, res) {
+  var body = req.body;
+  console.log(body);
 
-//   var sql = 'INSERT INTO user VALUES(?, ?, ?, ?,NOW())';
-//   var params = [body.id, body.pw, body.name,body.email,body.created];
-//   console.log(sql);
-//   conn.query(sql, params, function(err) {
-//       if(err) console.log('query is not excuted. insert fail...\n' + err);
-//       else res.redirect('/list');
-//   });
-// });
+  var sql = 'INSERT INTO user VALUES(?, ?, ?, ?,NOW())';
+  var params = [body.id, body.pw, body.name,body.email,body.created];
+  console.log(sql);
+  conn.query(sql, params);
+});
 
 module.exports = router;
