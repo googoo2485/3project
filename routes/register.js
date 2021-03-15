6f06001fe1aeed5b2f44c1f12be74d0eefac4c83
googoo2485 <googoo2485@github.com>
2021-03-15 17:43:02 +0900

    no message

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const session = require('express-session');
+const session = require('./session');
 const client = require('./mysql');
 const bcrypt = require('bcryptjs');
 
@@ -22,48 +22,36 @@ router.get('/', function(req, res, next) {
 // }
 
 client.connect();
+
+
 router.post('/', function (req, res) {
   let body = req.body;
   console.log(body);
-  // body.pw =bcrypt.hash(body.pw,12);
-  // bcrypt.genSalt(10,function(err,salt){
-  //   if(err){
-  //     console.log('bcrypt.genSalt() errer :',err.message);
-  //   }
-  //   else{
-  //     bcrypt.hash(plainTextPassword,salt,null,function(err,hash){
-  //       if(err){console.log('bcrypt.hash() errer:',err.message);}
-  //       else{console.log(hash);}
-  //     });
-  //   }
-// ?
-  // });
-
   
-  router.post('/checkId', (req, res) =>{
-    let input_id = req.body.data;
-    client.query('select * from user_table where id=?',[input_id],(err,data)=>{
-        console.log(err);
-        console.log('data ==>',data);
-
-        if(data.length == 0){
-            res.send({
-              re_result: true
-            });
-            console.log('사용 가능 ID');
-        }else{
-            res.send({re_result:false});
-            console.log('사용 불가 ID');
-        }
-    });
-});
-
-
   let sql = 'INSERT INTO user (id,pw,email,created) VALUES(?, ?, ?,NOW())';
   console.log(sql);
   client.query(sql,[body.id ,body.pw ,body.email ,body.created],()=>{
     console.log('????')
     res.redirect('login');
+  });
+});
+
+
+router.post('/checkId', (req, res) =>{
+  let input_id = req.body.data;
+  client.query('select * from user where id=?',[input_id],(err,data)=>{
+      console.log(err);
+      console.log('data ==>',data);
+
+      if(data.length == 0){
+          res.send({
+            re_result: true
+          });
+          console.log('사용 가능 ID');
+      }else{
+          res.send({re_result:false});
+          console.log('사용 불가 ID');
+      }
   });
 });
 
